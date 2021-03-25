5d918bfbe839cc660dea551f0a436495f8511d49
allissonconsorte04 <80799005+allissonconsorte04@users.noreply.github.com>
2021-03-24 21:30:07 -0300

    Add files via upload

---
+++ Trabalho1.java
@@ -0,0 +1,64 @@
+package br.com.dev2me.trabalho1;
+
+import javax.swing.JOptionPane;
+
+public class Trabalho1 {
+
+	public static void main(String[] args) {
+		int opcao = 0;
+		int voto = 0;
+		int angela = 0;
+		int gean = 0;
+		int cesar = 0;
+		int nulo = 0;
+		int branco = 0;
+		int quantidade = 0;
+	       do {  
+	    	   opcao = Integer.parseInt(JOptionPane.showInputDialog("Digite uma opção:"
+            		   + "- 1) Preparar Votação"
+            		   + "2) Finalizar a votação"
+            		   + "9) Sair do programa."));
+	    	
+	    	if (opcao == 1) {
+	    		voto = Integer.parseInt(JOptionPane.showInputDialog("Escolha seu candidato:"
+	    				+ "1 - Angela Pepino - PMDB; "
+	    				+ "2 - Gean da Silva - PT; "
+	    				+ "3 - Cesar Souza Neto - DEM; "
+	    				+ "4 - Nulo; "
+	    				+ "5 - Branco"));
+	    		
+	    		if (voto == 1) {
+	    			angela ++;
+	    		}
+	    		else if (voto == 2) {
+	    			gean ++;
+	    		}
+	    		else if (voto == 3) {
+	    			cesar ++;
+	    		}
+	    		else if (voto == 5) {
+	    			branco ++;
+	    		}
+	    		else {
+	    			nulo ++;
+	    		}
+	    		quantidade ++;
+	    	}
+	    	
+	    	if (opcao == 2) {
+	    		System.out.println("Quantidade de votos em cada candidato:");
+	    		System.out.println("1 - Angela Pepino - PMDB: " +angela);
+	    		System.out.println("2 - Gean da Silva - PT: " +gean);
+	    		System.out.println("3 - Cesar Souza Neto - DEM: " +cesar);
+	    		System.out.println("4 - Votos Nulos: " +nulo);
+	    		System.out.println("5 - Votos em branco: " +branco);
+	    		System.out.println(" ");
+	    		System.out.println("Quantidade de eleitores:" +quantidade);
+	    	}	    	
+	    } 
+while(opcao != 9);
+	    
+	    System.out.println("Programa finalizado.");
+	}
+
+	}
